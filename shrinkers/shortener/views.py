2d81a4e4c97a2cad25cef237e44e73f1fe350f92
kikkd <whddls6666@naver.com>
2022-09-27 10:49:09 +0900

    url1

--- shrinkers/shortener/views.py
+++ shrinkers/shortener/views.py
@@ -5,14 +5,15 @@ from django.shortcuts import redirect, render
 
 
 def index(request):
-    user = Users.objects.filter(username="admin").first()
+    # user = Users.objects.get(username="admin") # get은 반드시 1개만 가져옴
+    user = Users.objects.filter(username="admin").first() # 어떤값이 들어올지 몰라 filter로 
     email = user.email if user else "Anonymous User!"
     print(email)
     print(request.user.is_authenticated)
     if request.user.is_authenticated is False:
         email = "Anonymous User!"
     print(email)
-    return render(request, "base.html", {"welcome_msg": "Hello FastCampus!"})
+    return render(request, "base.html", {"welcome_msg": f"Hello {email}FastCampus!","hello" : "world"})
 
 
 def redirect_test(request):
